854c6c7617cfa790745aacfa762ff8d5c26c4420
Pedro Santana <134099838+pasantanav@users.noreply.github.com>
2023-07-17 20:25:52 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -2,10 +2,25 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
+function random(num) {
+  return Math.floor(Math.random() * num);
+}
+function generateCard() {
+  let card = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+  let cardNum = random(13);
+  let suitList = ["♦", "♥", "♠", "♣"];
+  let suitNum = random(4);
+  let color = suitNum < 2 ? "red" : "black";
+  let numberElm = document.getElementById("number");
+  let suitUpElm = document.getElementById("suitup");
+  let suitDownElm = document.getElementById("suitdown");
+  suitUpElm.innerText = suitList[suitNum];
+  suitUpElm.style.color = color;
+  suitDownElm.innerText = suitList[suitNum];
+  suitDownElm.style.color = color;
+  numberElm.innerText = card[cardNum];
+}
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  generateCard();
+  document.getElementById("card").addEventListener("click", generateCard);
 };
